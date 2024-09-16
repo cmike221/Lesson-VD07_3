b7bed3ee32791edf9f06a2900ddb7eff1714174b
cmike221 <cmike221@yandex.ru>
2024-09-16 12:45:02 +0300

    Первая версия без второго поля ввода пароля

---
+++ app/routes.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from . import db
+from .models import User
+from flask_login import login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('login.html')
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('main.profile'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html')
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        # Используем 'pbkdf2:sha256' вместо 'sha256'
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Your account has been created!', 'success')
+        return redirect(url_for('main.login'))
+    return render_template('register.html')
+
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.login'))
